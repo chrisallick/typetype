44d63fba79e099a209bcd3782421e8ee6c5bd4e9
Chris Allick <chrisallick@gmail.com>
2012-10-28 11:25:06 -0700

    games goes forever now.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 var audio = {};
-var tracks = ["a","b","c","correct","incorrect", "instructions"];
+var to_load = ["a","b","c","correct","incorrect","getstarted"];
 
+var instructions = ["getstarted"];
+var answers = ["correct","incorrect"];
+var letters = ["a","b","c"];
 $(document).ready(function() {
-    for( var i = 0, len = tracks.length; i < len; i++ ) {
-        audio[tracks[i]] = new Audio();
-        audio[tracks[i]].setAttribute("src","jams/"+tracks[i]+".m4a");
-        audio[tracks[i]].addEventListener("canplaythrough",onLoadedHandler,false);
-        audio[tracks[i]].addEventListener("ended",onEndedHandler,false);
-        audio[tracks[i]].load();
+    for( var i = 0, len = to_load.length; i < len; i++ ) {
+        audio[to_load[i]] = new Audio();
+        audio[to_load[i]].setAttribute("src","jams/"+to_load[i]+".m4a");
+        audio[to_load[i]].addEventListener("canplaythrough",onLoadedHandler,false);
+        audio[to_load[i]].addEventListener("ended",onEndedHandler,false);
+        audio[to_load[i]].load();
     }
 
     $("#form").css({
@@ -18,43 +21,97 @@ $(document).ready(function() {
     document.form.textarea.focus();
 });
 
+
+/*
+    array to check item agains
+    return true if item in array, false if not
+*/
+doesInclude = function( array, item ) {
+    return (array.indexOf(item) != -1);
+}
+
+/*
+    set loaded to 0 and count up as audio is loaded
+    if they are all loaded, play the first letter
+*/
 var loaded = 0;
 var playing = "";
 onLoadedHandler = function(event) {
     loaded++;
-    if( loaded == tracks.length ) {
-        playing = "instructions";
-        audio["instructions"].play();
+    if( loaded == to_load.length ) {
+        playing = "getstarted";
+        audio["getstarted"].play();
     }
 }
 
+/*
+    t - timeout to play next
+
+    if you just read the instructions
+        play the next letter in half a second
+    if it's a letter
+        check to see if thye got it correct
+*/
 var t;
 onEndedHandler = function(event) {
     clearTimeout(t);
-    if( playing == "instructions" ) {
+    if( playing == "getstarted" ) {
         t = setTimeout( playNext, 500 );
-    } else if( playing == "a" ) {
+    } else if( playing == "correct" ) {
+        t = setTimeout( playNext, 1000 );
+    } else if( doesInclude(letters,playing) ) {
         ct = setTimeout( checkInput, 250 );
     }
     playing = "";
 }
 
+/*
+    if the value is correct, end the round
+*/
+roundOver = function() {
+
+}
+
+/*
+    ct - how often to check the input
+    redo_counter - when to re-read instructions to player
+
+    clear the ct timeout
+    is it correct
+        play correct
+    is if empty?
+        wait and check again
+    is it wrong?
+        play incorrect
+    set value of input to ""
+*/
 var ct;
 var redo_counter;
 checkInput = function() {
     clearTimeout( ct );
-    if( $("#textarea").val() == "a" ) {
-        audio["correct"].play();
+    if( $("#textarea").val() == waiting_for ) {
+        waiting_for = "";
+        playing = "correct";
+        audio[playing].play();
     } else if( $("#textarea").val() == "" ) {
         ct = setTimeout( checkInput, 1000 );
-    } else if( $("#textarea").val() != "a" ) {
+    } else if( $("#textarea").val() != waiting_for ) {
         audio["incorrect"].play();
         ct = setTimeout( checkInput, 1000 );
     }
     $("#textarea").val("");
 }
 
+/*
+    next - random value from 0 - length of letters array
+
+    pick a random letter
+    set playing to that letter
+    play it
+*/
+var waiting_for = "";
 playNext = function() {
-    playing = "a";
-    audio["a"].play();
+    var next = Math.floor(Math.random()*letters.length);
+    waiting_for = playing = letters[next];
+    audio[playing].play();
 }
